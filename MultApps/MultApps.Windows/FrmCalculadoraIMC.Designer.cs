b325a65e86a398cfebb9139b5e689e68f5cd5c14
Ana Carolina <senac.anacarolinaribeiro@gmail.com>
2025-02-27 21:53:57 -0300

    chore: fazendo a construção de IMC #3

--- MultApps/MultApps.Windows/FrmCalculadoraIMC.Designer.cs
+++ MultApps/MultApps.Windows/FrmCalculadoraIMC.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Selecione = new System.Windows.Forms.Label();
+            this.chkAdulto = new System.Windows.Forms.CheckBox();
+            this.chkCrianca = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.chkCrianca);
+            this.panel1.Controls.Add(this.chkAdulto);
+            this.panel1.Controls.Add(this.Selecione);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 426);
+            this.panel1.TabIndex = 1;
+            // 
+            // Selecione
+            // 
+            this.Selecione.AutoSize = true;
+            this.Selecione.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Selecione.Location = new System.Drawing.Point(25, 17);
+            this.Selecione.Name = "Selecione";
+            this.Selecione.Size = new System.Drawing.Size(88, 20);
+            this.Selecione.TabIndex = 0;
+            this.Selecione.Text = "Selecione";
+            // 
+            // chkAdulto
+            // 
+            this.chkAdulto.AutoSize = true;
+            this.chkAdulto.BackColor = System.Drawing.Color.White;
+            this.chkAdulto.Checked = true;
+            this.chkAdulto.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAdulto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkAdulto.ForeColor = System.Drawing.Color.Gold;
+            this.chkAdulto.Location = new System.Drawing.Point(29, 60);
+            this.chkAdulto.Name = "chkAdulto";
+            this.chkAdulto.Size = new System.Drawing.Size(68, 22);
+            this.chkAdulto.TabIndex = 2;
+            this.chkAdulto.Text = "Adulto";
+            this.chkAdulto.UseVisualStyleBackColor = false;
+            this.chkAdulto.CheckedChanged += new System.EventHandler(this.chkAdulto_CheckedChanged);
+            // 
+            // chkCrianca
+            // 
+            this.chkCrianca.AutoSize = true;
+            this.chkCrianca.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkCrianca.Location = new System.Drawing.Point(159, 60);
+            this.chkCrianca.Name = "chkCrianca";
+            this.chkCrianca.Size = new System.Drawing.Size(78, 22);
+            this.chkCrianca.TabIndex = 3;
+            this.chkCrianca.Text = "Criança";
+            this.chkCrianca.UseVisualStyleBackColor = true;
+            this.chkCrianca.CheckedChanged += new System.EventHandler(this.chkCrianca_CheckedChanged);
+            // 
+            // FrmCalculadoraIMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmCalculadoraIMC";
             this.Text = "FrmCalculadoraIMC";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Selecione;
+        private System.Windows.Forms.CheckBox chkCrianca;
+        private System.Windows.Forms.CheckBox chkAdulto;
     }
 }
