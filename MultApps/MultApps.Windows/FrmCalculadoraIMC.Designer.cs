4b96f06fc5140353b26c41f079e41f2aa53d6f11
Ana Carolina <senac.anacarolinaribeiro@gmail.com>
2025-03-11 19:19:39 -0300

    feat: codigo calculadora IMC homem #3

--- MultApps/MultApps.Windows/FrmCalculadoraIMC.Designer.cs
+++ MultApps/MultApps.Windows/FrmCalculadoraIMC.Designer.cs
@@ -29,19 +29,32 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblIdade = new System.Windows.Forms.Label();
             this.chkFeminino = new System.Windows.Forms.CheckBox();
             this.chkMasculino = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
             this.chkCrianca = new System.Windows.Forms.CheckBox();
             this.chkAdulto = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.lblIdade = new System.Windows.Forms.Label();
+            this.panelAdulto = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.cbmIdade = new System.Windows.Forms.ComboBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultadoImc = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panelAdulto.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblResultadoImc);
+            this.panel1.Controls.Add(this.btnCalcular);
+            this.panel1.Controls.Add(this.panelAdulto);
             this.panel1.Controls.Add(this.lblIdade);
             this.panel1.Controls.Add(this.chkFeminino);
             this.panel1.Controls.Add(this.chkMasculino);
@@ -49,11 +62,24 @@
             this.panel1.Controls.Add(this.chkCrianca);
             this.panel1.Controls.Add(this.chkAdulto);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(28, 28);
+            this.panel1.Location = new System.Drawing.Point(29, 28);
             this.panel1.Margin = new System.Windows.Forms.Padding(6);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1549, 759);
             this.panel1.TabIndex = 0;
+            this.panel1.UseWaitCursor = true;
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdade.Location = new System.Drawing.Point(73, 143);
+            this.lblIdade.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(184, 25);
+            this.lblIdade.TabIndex = 6;
+            this.lblIdade.Text = "Acima de 19 anos";
+            this.lblIdade.UseWaitCursor = true;
             // 
             // chkFeminino
             // 
@@ -66,6 +92,7 @@
             this.chkFeminino.TabIndex = 5;
             this.chkFeminino.Text = "Feminino";
             this.chkFeminino.UseVisualStyleBackColor = true;
+            this.chkFeminino.UseWaitCursor = true;
             // 
             // chkMasculino
             // 
@@ -80,6 +107,7 @@
             this.chkMasculino.TabIndex = 4;
             this.chkMasculino.Text = "Masculino";
             this.chkMasculino.UseVisualStyleBackColor = true;
+            this.chkMasculino.UseWaitCursor = true;
             // 
             // label2
             // 
@@ -91,6 +119,7 @@
             this.label2.Size = new System.Drawing.Size(61, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Sexo";
+            this.label2.UseWaitCursor = true;
             // 
             // chkCrianca
             // 
@@ -103,6 +132,7 @@
             this.chkCrianca.TabIndex = 2;
             this.chkCrianca.Text = "Criança";
             this.chkCrianca.UseVisualStyleBackColor = true;
+            this.chkCrianca.UseWaitCursor = true;
             this.chkCrianca.Click += new System.EventHandler(this.chkCrianca_CheckedChanged);
             // 
             // chkAdulto
@@ -118,6 +148,7 @@
             this.chkAdulto.TabIndex = 1;
             this.chkAdulto.Text = "Adulto";
             this.chkAdulto.UseVisualStyleBackColor = true;
+            this.chkAdulto.UseWaitCursor = true;
             this.chkAdulto.Click += new System.EventHandler(this.chkAdulto_CheckedChanged);
             // 
             // label1
@@ -130,17 +161,117 @@
             this.label1.Size = new System.Drawing.Size(107, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Selecione";
+            this.label1.UseWaitCursor = true;
             // 
-            // lblIdade
+            // panelAdulto
             // 
-            this.lblIdade.AutoSize = true;
-            this.lblIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIdade.Location = new System.Drawing.Point(73, 143);
-            this.lblIdade.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.lblIdade.Name = "lblIdade";
-            this.lblIdade.Size = new System.Drawing.Size(184, 25);
-            this.lblIdade.TabIndex = 6;
-            this.lblIdade.Text = "Acima de 19 anos";
+            this.panelAdulto.Controls.Add(this.cbmIdade);
+            this.panelAdulto.Controls.Add(this.txtAltura);
+            this.panelAdulto.Controls.Add(this.txtPeso);
+            this.panelAdulto.Controls.Add(this.label5);
+            this.panelAdulto.Controls.Add(this.label4);
+            this.panelAdulto.Controls.Add(this.label3);
+            this.panelAdulto.Location = new System.Drawing.Point(78, 194);
+            this.panelAdulto.Name = "panelAdulto";
+            this.panelAdulto.Size = new System.Drawing.Size(540, 131);
+            this.panelAdulto.TabIndex = 7;
+            this.panelAdulto.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Peso";
+            this.label3.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(145, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Altura";
+            this.label4.UseWaitCursor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(269, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 24);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Idade";
+            this.label5.UseWaitCursor = true;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(11, 65);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 29);
+            this.txtPeso.TabIndex = 9;
+            this.txtPeso.UseWaitCursor = true;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(134, 65);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 29);
+            this.txtAltura.TabIndex = 10;
+            this.txtAltura.UseWaitCursor = true;
+            // 
+            // cbmIdade
+            // 
+            this.cbmIdade.FormattingEnabled = true;
+            this.cbmIdade.Items.AddRange(new object[] {
+            "1 ano",
+            "2 anos",
+            "3 anos",
+            "4 anos",
+            "5 anos",
+            "6 anos",
+            "7 anos",
+            "8 anos",
+            "9 anos",
+            "10 anos",
+            "11 anos",
+            "12 anos",
+            "13 anos",
+            "14 anos",
+            "15 anos",
+            "16 anos",
+            "17 anos",
+            "18 anos"});
+            this.cbmIdade.Location = new System.Drawing.Point(273, 65);
+            this.cbmIdade.Name = "cbmIdade";
+            this.cbmIdade.Size = new System.Drawing.Size(121, 32);
+            this.cbmIdade.TabIndex = 11;
+            this.cbmIdade.UseWaitCursor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnCalcular.Location = new System.Drawing.Point(78, 352);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(111, 35);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultadoImc
+            // 
+            this.lblResultadoImc.AutoSize = true;
+            this.lblResultadoImc.Location = new System.Drawing.Point(284, 357);
+            this.lblResultadoImc.Name = "lblResultadoImc";
+            this.lblResultadoImc.Size = new System.Drawing.Size(94, 24);
+            this.lblResultadoImc.TabIndex = 9;
+            this.lblResultadoImc.Text = "Resultado";
             // 
             // FrmCalculadoraIMC
             // 
@@ -155,6 +286,8 @@
             this.Text = "FrmCalculadoraIMC";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panelAdulto.ResumeLayout(false);
+            this.panelAdulto.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -169,5 +302,14 @@
         private System.Windows.Forms.CheckBox chkMasculino;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.Panel panelAdulto;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbmIdade;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultadoImc;
     }
 }
--- MultApps/MultApps.Windows/FrmCalculadoraIMC.cs
+++ MultApps/MultApps.Windows/FrmCalculadoraIMC.cs
@@ -23,6 +23,8 @@ namespace MultApps.Windows
             chkAdulto.ForeColor = Color.Gray;
             chkAdulto.Checked = false;
             lblIdade.Text = "Abaixo de 19 anos";
+            cbmIdade.Visible = true;
+            lblIdade.Visible = true;
         }
 
         private void chkAdulto_CheckedChanged(object sender, EventArgs e)
@@ -31,6 +33,52 @@ namespace MultApps.Windows
             chkCrianca.ForeColor = Color.Gray;
             chkCrianca.Checked = false;
             lblIdade.Text = "Acima de 19 anos";
+            cbmIdade.Visible = false;
+            lblIdade.Visible= false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            var peso = double.Parse(txtPeso.Text);
+            var altura = double.Parse(txtAltura.Text);
+
+            var imc = peso / (altura * altura);
+            
+            lblResultadoImc.Text = imc.ToString("N2");
+
+            if(imc <= 18.5)
+            {
+                lblResultadoImc.Text = $@"Seu IMC: {imc:N2} é abaixo do normal";
+            }
+            else if(imc < 24.9)
+            {
+                lblResultadoImc.Text = $@"Seu IMC: {imc:N2} é normal";
+            }
+            else if(imc <29.9)
+            {
+                lblResultadoImc.Text = $@"Seu IMC: {imc:N2} é sobrepeso";
+
+            }
+            else if (imc < 34.9)
+            {
+                lblResultadoImc.Text = $@"Seu IMC: {imc:N2} é obesidade grau 1";
+
+            }
+            else if (imc < 39.9)
+            {
+                lblResultadoImc.Text = $@"Seu IMC: {imc:N2} é obesidade grau 2";
+
+            }
+            else 
+            {
+                lblResultadoImc.Text = $@"Seu IMC: {imc:N2} é obesidade grau 3";
+
+            }
         }
     }
 }
