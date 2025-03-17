a21b3d0ded6fba96c568e516e344f7d6d4e8abfc
Ana Carolina <senac.anacarolinaribeiro@gmail.com>
2025-03-17 19:31:35 -0300

    feat: terminei o designer do splash screen. #5

--- MultApps/MultApps.Windows/Principal.cs
+++ MultApps/MultApps.Windows/Principal.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace MultApps.Windows
 {
@@ -21,6 +23,30 @@ namespace MultApps.Windows
         {
             var form = new FrmCalculadoraIMC();
             form.ShowDialog();  
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Principal_Shown(object sender, EventArgs e)
+        {
+            this.Refresh();
+            for (int i = 0; i < 101; i++)
+            {
+                progressBar1.Value = i;
+                Thread.Sleep(40);
+            }
+            progressBar1.Value = 99;
+            Thread.Sleep(500);
+
+            this.Close();
+        }
+        private void MDIPrincipal_Shown(object sender, EventArgs e)
+        {
+            var loading = new SplashScreen();
+            loading.ShowDialog();
         }
     }
 }
