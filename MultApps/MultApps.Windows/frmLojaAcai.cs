043a4727774c54aa903a33b197cb42cc451f5c9d
Ana Carolina <senac.anacarolinaribeiro@gmail.com>
2025-03-21 21:50:00 -0300

    feat: criei o designer da loja de acai #9

---
+++ MultApps/MultApps.Windows/frmLojaAcai.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultApps.Windows
+{
+    public partial class frmLojaAcai : Form
+    {
+        public frmLojaAcai()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
